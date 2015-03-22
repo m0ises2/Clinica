9811d0a08ce70384369dfa59701099fc7b251f75
Moisés Alejandro <moisesalvarado84@gmail.com>
2015-03-21 19:42:40 -0430

    Actualizada la vista mostrar, agregadaa la tabla responsiva y con buscado, pagination y organizadores.

--- proyecto/app/cache/dev/twig/ad/bf/020a74b0251c3c895c46f23d8ae8f29d2824998bc3606798b956eab970a1.php
+++ proyecto/app/cache/dev/twig/ad/bf/020a74b0251c3c895c46f23d8ae8f29d2824998bc3606798b956eab970a1.php
@@ -18,8 +18,10 @@ class __TwigTemplate_adbf020a74b0251c3c895c46f23d8ae8f29d2824998bc3606798b956eab
         }
 
         $this->blocks = array(
+            'styles' => array($this, 'block_styles'),
             'title' => array($this, 'block_title'),
             'content' => array($this, 'block_content'),
+            'javascript' => array($this, 'block_javascript'),
         );
     }
 
@@ -34,15 +36,36 @@ class __TwigTemplate_adbf020a74b0251c3c895c46f23d8ae8f29d2824998bc3606798b956eab
     }
 
     // line 3
+    public function block_styles($context, array $blocks = array())
+    {
+        // line 4
+        echo "\t";
+        $this->displayParentBlock("styles", $context, $blocks);
+        echo "
+\t<!-- DataTables Responsive CSS -->
+    <link href=\"";
+        // line 6
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/eam/css/datatablesresponsive/css/dataTables.responsive.css"), "html", null, true);
+        echo "\" rel=\"stylesheet\" type=\"text/css\" />
+    
+    <!-- DataTables CSS -->
+    <link href=\"";
+        // line 9
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/eam/css/datatablesplugins/integration/bootstrap/3/dataTables.bootstrap.css"), "html", null, true);
+        echo "\" rel=\"stylesheet\" type=\"text/css\" />
+";
+    }
+
+    // line 12
     public function block_title($context, array $blocks = array())
     {
         echo "  Mostrar ";
     }
 
-    // line 5
+    // line 14
     public function block_content($context, array $blocks = array())
     {
-        // line 6
+        // line 15
         echo "
 \t<div class=\"row\">
 \t\t<div class=\"col-lg-12\">
@@ -50,11 +73,11 @@ class __TwigTemplate_adbf020a74b0251c3c895c46f23d8ae8f29d2824998bc3606798b956eab
 \t\t</div>
 \t</div>
 \t<div class = \"row\">
-\t\t<div class = \"col-lg-8 col-lg-offset-2\">
+\t\t<div class = \"col-lg-10 col-lg-offset-1\">
 \t\t\t<div class = \"panel panel-info\" >
 \t\t\t\t<div class = \"panel-body\" >
-\t\t\t\t\t<div class = \"table-responsive\">
-\t\t\t\t\t\t<table class = \"table\" >
+\t\t\t\t\t<div class = \"dataTable_wrapper\">
+\t\t\t\t\t\t<table class=\"table table-striped table-bordered table-hover\" id=\"dataTables-example\" >
 \t\t\t\t\t\t\t<thead>
 \t\t\t\t\t\t\t\t<tr>
 \t\t\t\t\t\t\t\t\t<th>Nombre</th>
@@ -66,41 +89,41 @@ class __TwigTemplate_adbf020a74b0251c3c895c46f23d8ae8f29d2824998bc3606798b956eab
 \t\t\t\t\t\t\t</thead>
 \t\t\t\t\t\t\t<tbody>
 \t\t\t\t\t\t\t\t";
-        // line 28
+        // line 37
         $context['_parent'] = (array) $context;
         $context['_seq'] = twig_ensure_traversable((isset($context["entidades"]) ? $context["entidades"] : $this->getContext($context, "entidades")));
         foreach ($context['_seq'] as $context["_key"] => $context["usuario"]) {
-            // line 29
-            echo "\t\t\t\t\t\t\t\t\t<tr>
+            // line 38
+            echo "\t\t\t\t\t\t\t\t\t<tr class=\"even gradeA\">
 \t\t\t\t\t\t\t\t\t\t
 \t\t\t\t\t\t\t\t\t\t<td> ";
-            // line 31
+            // line 40
             echo twig_escape_filter($this->env, $this->getAttribute($context["usuario"], "nombre", array()), "html", null, true);
             echo " </td>
 \t\t\t\t\t\t\t\t\t\t<td> ";
-            // line 32
+            // line 41
             echo twig_escape_filter($this->env, $this->getAttribute($context["usuario"], "apellido", array()), "html", null, true);
             echo " </td>
 \t\t\t\t\t\t\t\t\t\t<td> ";
-            // line 33
+            // line 42
             echo twig_escape_filter($this->env, $this->getAttribute($context["usuario"], "nombreUsuario", array()), "html", null, true);
             echo " </td>
 \t\t\t\t\t\t\t\t\t\t<td> ";
-            // line 34
+            // line 43
             echo twig_escape_filter($this->env, $this->getAttribute($context["usuario"], "tipo", array()), "html", null, true);
             echo " </td>
 \t\t\t\t\t\t\t\t\t\t<td>
 \t\t\t\t\t\t\t\t\t\t\t<div>
 \t\t\t\t\t\t\t\t\t\t\t\t<form method=\"POST\" action=\"";
-            // line 37
+            // line 46
             echo $this->env->getExtension('routing')->getPath("Ver");
             echo "\">
 \t\t\t\t\t\t\t\t\t\t\t\t\t<input type=\"hidden\" value=\"";
-            // line 38
+            // line 47
             echo twig_escape_filter($this->env, $this->getAttribute($context["usuario"], "id", array()), "html", null, true);
             echo "\" name=\"id\" >
 \t\t\t\t\t\t\t\t\t\t\t\t\t<button class=\"btn btn-outline btn-info\" type=\"submit\">Detalles</button>
-\t\t\t\t\t\t\t\t\t\t\t\t</form>
+\t\t\t\t\t\t\t\t\t\t\t\t</form>\t\t\t\t\t\t\t\t\t\t\t\t
 \t\t\t\t\t\t\t\t\t\t\t</div>
 \t\t\t\t\t\t\t\t\t\t\t
 \t\t\t\t\t\t\t\t\t\t</td>\t\t\t\t\t\t\t\t\t\t
@@ -110,7 +133,7 @@ class __TwigTemplate_adbf020a74b0251c3c895c46f23d8ae8f29d2824998bc3606798b956eab
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['usuario'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 46
+        // line 55
         echo "\t\t\t\t\t\t\t</tbody>
 \t\t\t\t\t\t</table>
 \t\t\t\t\t</div>
@@ -118,6 +141,34 @@ class __TwigTemplate_adbf020a74b0251c3c895c46f23d8ae8f29d2824998bc3606798b956eab
 \t\t\t</div>
 \t\t</div>
 \t</div>
+";
+    }
+
+    // line 64
+    public function block_javascript($context, array $blocks = array())
+    {
+        // line 65
+        echo "\t";
+        $this->displayParentBlock("javascript", $context, $blocks);
+        echo "
+
+\t<script src=\"";
+        // line 67
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/eam/js/jquery.dataTables.min.js"), "html", null, true);
+        echo "\"></script>
+\t<script src=\"";
+        // line 68
+        echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/eam/js/dataTables.bootstrap.min.js"), "html", null, true);
+        echo "\"></script>
+
+\t<script>
+    \$(document).ready(function() {
+        \$('#dataTables-example').DataTable({
+                responsive: true
+        });
+    });
+    </script>
+
 ";
     }
 
@@ -133,6 +184,6 @@ class __TwigTemplate_adbf020a74b0251c3c895c46f23d8ae8f29d2824998bc3606798b956eab
 
     public function getDebugInfo()
     {
-        return array (  114 => 46,  100 => 38,  96 => 37,  90 => 34,  86 => 33,  82 => 32,  78 => 31,  74 => 29,  70 => 28,  46 => 6,  43 => 5,  37 => 3,  11 => 1,);
+        return array (  161 => 68,  157 => 67,  151 => 65,  148 => 64,  137 => 55,  123 => 47,  119 => 46,  113 => 43,  109 => 42,  105 => 41,  101 => 40,  97 => 38,  93 => 37,  69 => 15,  66 => 14,  60 => 12,  54 => 9,  48 => 6,  42 => 4,  39 => 3,  11 => 1,);
     }
 }
